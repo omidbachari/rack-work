e3d0a657a67b02518f55fbcb99552eef8c7816e1
Omid Bachari <omid.bachari@launchacademy.co>
2014-12-05 17:20:54 -0500

    Removed comments

--- app.rb
+++ app.rb
@@ -11,24 +11,3 @@ app = Proc.new do |env|
 end
 
 Rack::Handler::WEBrick.run app
-
-
-#app = Proc.new { [200, {}, ["str"]] }
-
-#
-# call
-# env
-# each
-
-#A rack application is an object. This object comes between Sinatra and the your server. I used the WEBrick server.
-
-#When WEBrick receives a request from your browser, WEBrick parses it and then it interacts with your rack application by doing the following:
-
-#Passing the parsed request into the object as the environment, calling
-
-
-#A Rack application is an Ruby object that responds to the method #call. It takes exactly one argument, the environment and returns an Array of exactly three values: The status, the headers, and the body.
-
-#Informally, a Rack application is a thing that responds to #call and takes a hash as argument, returning an array of status, headers and a body.
-
-#The best way to describe a rack application is to understand what it does.
